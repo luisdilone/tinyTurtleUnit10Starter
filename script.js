3af004bbadaf8b13d102cd0b8de8f79a13f7b363
Luis Angel Dilone <ldilone@aclanyc.com>
2017-03-22 12:36:03 +0000

    Make Super Shape

--- script.js
+++ script.js
@@ -1,18 +1,75 @@
 TinyTurtle.apply(window, [undefined, 400, 400]);
 
-function square(){
-forward(50);
+function square(size){
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 right(90);
-forward(50);
+forward(size);
 }
+function triangle(size){
+right(30);
+forward(size);
+right(120);
+forward(size);
+right(120);
+forward(size);
+}
+function house(size){
+square(size);
+triangle(size);
+}
+function rectangle(sizeLength, sizeWidth){
+forward(sizeLength);
+right(90);
+forward(sizeWidth);
+right(90);
+forward(sizeLength);
+right(90);
+forward(sizeWidth);
+right(90);
+forward(sizeLength); 
+}
+function pentagon(size){
+right(90);
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+right(72);
+forward(size);
+}
+function hexagon(size){
+right(90);
+forward(size);
+right(60);
+forward(size);
+right(60);
+forward(size);
+right(60);
+forward(size);
+right(60);
+forward(size);
+right(60);
+forward(size);
+}
+function superShape(size){
+square(size);
+triangle(size);
+right(72);
+pentagon(size);
+right(60);
+hexagon(size);
 
+}
 // Type your function call below
 
-
+superShape(50);
 stamp();
